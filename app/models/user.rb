e7b0be406f33b2e571b9a0efe7fc4499b405a337
Lexie <Alexisnfrisch@gmail.com>
2019-04-18 20:04:28 -0400

    Added responses controller for comment responses

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   
   has_many :comments
+  has_many :responses
+
+  has_one :avatar
+  mount_uploader :avatar, AvatarUploader
+
+  validates :username, presence: true
+  validates :username, uniqueness: true
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_18_234300) do
+ActiveRecord::Schema.define(version: 2019_04_18_235803) do
 
   create_table "comments", force: :cascade do |t|
+    t.string "message"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "responses", force: :cascade do |t|
     t.string "message"
     t.integer "user_id"
     t.datetime "created_at", null: false
