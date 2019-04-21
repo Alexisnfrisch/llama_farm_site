232de31b816436ace1ca4106330634897f873083
Lexie <Alexisnfrisch@gmail.com>
2019-04-21 02:41:31 -0400

    Events and Create Events good, All Users and Your Profile show fine, Login and Validations Function Properly, Some formatting ok

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,18 @@
 module HomeHelper
-end
+	
+
+	# def bookings
+ #    @bookings = Booking.all.order('created_at DESC')
+ #    @booking = Booking.new
+ #    @booked_events = []
+
+ #    Booking.all.each do |booking|
+ #      if current_user.booked_events.include?(booking.user_id) || current_user.id == booking.user_id
+ #        @booked_events.push(booking)
+ #      end
+ #    end
+ #    @sorted_booked_events = @booked_events.sort_by(&:created_at).reverse
+
+  end
+
+ 
